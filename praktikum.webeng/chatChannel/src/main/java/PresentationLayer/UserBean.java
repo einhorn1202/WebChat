b89c7d96780013551b2d2c849ad431ea213d1401
lennart.meyer <lennart.meyer@study.hs-dueseldorf.de>
2020-06-18 19:19:01 +0200

    Klasse UserBean berichtigt (Sorry Lena)
    PS : SCHEISS BONN

--- praktikum.webeng/chatChannel/src/main/java/PresentationLayer/UserBean.java
+++ praktikum.webeng/chatChannel/src/main/java/PresentationLayer/UserBean.java
@@ -3,76 +3,43 @@ package PresentationLayer;
 import java.awt.Color;
 import java.awt.Image;
 
+import javax.annotation.PostConstruct;
+
+import BusinessLogicLayer.ChannelManager;
+import BusinessLogicLayer.UserManager;
+import TransferObjects.Channel;
 import TransferObjects.User;
 
 public class UserBean {
+	
+	User user;
+	UserManager userManager;
+	
+	@PostConstruct
+	public void init() {
+		if(userManager == null) {
+			userManager = new UserManager();
+		}
+		if(user == null) {
+			user = new User();
+		}
+	}
 
-    User user;
-    int user_id;
-    String username;
-    String password;
-    Image profile_pic;
-    String color;
+	
+	public User getUser() {
+		return user;
+	}
 
-    public UserBean() {
-        // TODO Auto-generated constructor stub
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
 
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser_id(int userId) {
-        this.user_id = userId;
-    }
-
-    public void setUsername(String username) {
-
-    }
-
-    public void setPassword() {
-
-    }
-
-    public void setProfile_pic() {
-
-    }
-
-    public void setColor() {
-
-    }
-
-    public int getID() {
-        return user_id;
-
-    }
-
-    public String getUsername() {
-        return username;
-
-    }
-
-    public String getPassword() {
-        return password;
-
-    }
-
-    public Image getProfile_pic() {
-        return profile_pic;
-
-    }
-
-    public Color getColor() {
-        return null;
-
-    }
-
-    public String save() {
-        return "";
-    }
-
+	/*
+	 * ACTION-METHODEN DER BEAN
+	 */
+	
+	
+    
 }
