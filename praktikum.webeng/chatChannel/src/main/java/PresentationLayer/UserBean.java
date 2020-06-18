f97fc9a078ea5b450d25b9f6407f72f76b3ddd04
Julius Jérôme <julius.potthoff@gmx.de>
2020-06-18 08:37:33 +0200

    Lenas Discord Commits

--- praktikum.webeng/chatChannel/src/main/java/PresentationLayer/UserBean.java
+++ praktikum.webeng/chatChannel/src/main/java/PresentationLayer/UserBean.java
@@ -1,9 +1,78 @@
 package PresentationLayer;
 
+import java.awt.Color;
+import java.awt.Image;
+
+import TransferObjects.User;
+
 public class UserBean {
 
-	public UserBean() {
-		// TODO Auto-generated constructor stub
-	}
+    User user;
+    int user_id;
+    String username;
+    String password;
+    Image profile_pic;
+    String color;
+
+    public UserBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser_id(int userId) {
+        this.user_id = userId;
+    }
+
+    public void setUsername(String username) {
+
+    }
+
+    public void setPassword() {
+
+    }
+
+    public void setProfile_pic() {
+
+    }
+
+    public void setColor() {
+
+    }
+
+    public int getID() {
+        return user_id;
+
+    }
+
+    public String getUsername() {
+        return username;
+
+    }
+
+    public String getPassword() {
+        return password;
+
+    }
+
+    public Image getProfile_pic() {
+        return profile_pic;
+
+    }
+
+    public Color getColor() {
+        return null;
+
+    }
+
+    public String save() {
+        return "";
+    }
 
 }
