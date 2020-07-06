96280a6fcbb135c325f1cd73ce3fcbd9b2809ae2
lennart.meyer <lennart.meyer@study.hs-dueseldorf.de>
2020-07-06 12:41:31 +0200

    Filter hinzugefügt aber ohne Funktion

---
+++ praktikum.webeng/chatChannel/src/main/java/Filters/FilterLoggedIn.java
@@ -0,0 +1,47 @@
+package Filter;
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import PresentationLayer.*;
+import javax.inject.*;
+
+@WebFilter(filterName="FilterLoggedIn", urlPatterns = {"/PROTECTED/*"})
+public class FilterLoggedIn implements javax.servlet.Filter {
+	
+	
+	@Inject
+	UserBean userBean;
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		((HttpServletResponse) response).sendError(403, "Sie sind nicht eingeloggt!");
+		// TODO Auto-generated method stub
+		//if(userBean.getUser().getUsername() == null)
+		//	((HttpServletResponse) response).sendError(403, "Sie sind nicht eingeloggt!");
+//		else {
+//			chain.doFilter(request, response);
+//		}
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
